223918e0777a491e69c84c21b9bf65cbd6f008d4
B251 <ba.sadler1@gmail.com>
2024-01-12 19:45:45 +0000

    current day info updates when search made including conversion for wind speed from m/s to kph

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,13 @@
 
-//script to update all the dates for the days using dayjs
+//script to update all dates for the days using dayjs
 
 const today = dayjs();
-const secondDate = today.add(1, 'day')
-const thirdDate = today.add(2, 'day')
-const forthDate = today.add(3, 'day')
-const fifthDate = today.add(4, 'day')
-const sixthDate = today.add(5, 'day')
+const secondDate = today.add(1, 'day');
+const thirdDate = today.add(2, 'day');
+const forthDate = today.add(3, 'day');
+const fifthDate = today.add(4, 'day');
+const sixthDate = today.add(5, 'day');
 
-$("#currentDate").text(" " + today.format("DD/MM/YYYY"));
 $("#secondDate").text(secondDate.format("DD/MM/YYYY"));
 $("#thirdDate").text(thirdDate.format("DD/MM/YYYY"));
 $("#forthDate").text(forthDate.format("DD/MM/YYYY"));
@@ -26,15 +25,18 @@ const APIKey = "c27b6f158393dd9e97b1661d03e463b8";
     
     const searchValue = $("#search-field").val().trim();
   
-   const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${APIKey}`;
+   const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${APIKey}&units=metric`;
 
     fetch(queryURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-  
+      $("#currentCity").text(data.name + " " + today.format("DD/MM/YYYY"));
+      $("#day1temp").text(data.main.temp);
+      $("#day1wind").text(((data.wind.speed)*3.6).toPrecision(2));
+      $("#day1humidity").text(data.main.humidity);
+      console.log(data)
       })
   
     });
@@ -48,3 +50,8 @@ const APIKey = "c27b6f158393dd9e97b1661d03e463b8";
 //when search using search button it passes value to fetch function which then interacts with API to retrieve data before updating html using jquery.
 
 //when search is pressed the past search buttons are populated, when past search is pressed it also calls function same as search button which runs function but instead passes value of name in field.
+
+
+//where do I get the future day look ahead
+
+//where do I get the images from, 
