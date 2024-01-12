e26d212a33af9fb331b989ba62e802c37b204208
B251 <ba.sadler1@gmail.com>
2024-01-12 14:44:14 +0000

    added some Pseudo code to script file

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+//when search using search button it passes value to fetch function which then interacts with API to retrieve data before updating html using jquery.
+
+//when search is pressed the past search buttons are populated, when past search is pressed it also calls function same as search button which runs function but instead passes value of name in field.
