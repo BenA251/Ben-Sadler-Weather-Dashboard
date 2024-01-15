4ad16da229925d626e74bda423289e9b9eb10163
B251 <ba.sadler1@gmail.com>
2024-01-15 21:01:30 +0000

    added ability to save local data

--- scripts/script.js
+++ scripts/script.js
@@ -8,12 +8,15 @@ const forthDate = today.add(3, 'day');
 const fifthDate = today.add(4, 'day');
 const sixthDate = today.add(5, 'day');
 
+$("#currentCity").text(today.format("DD/MM/YYYY"));
 $("#secondDate").text(secondDate.format("DD/MM/YYYY"));
 $("#thirdDate").text(thirdDate.format("DD/MM/YYYY"));
 $("#forthDate").text(forthDate.format("DD/MM/YYYY"));
 $("#fifthDate").text(fifthDate.format("DD/MM/YYYY"));
 $("#sixthDate").text(sixthDate.format("DD/MM/YYYY"));
 
+var searchIdArr = [1,2,3,4,5,6];
+var counterValue;
 
 //script for accessing API
 
@@ -50,7 +53,11 @@ const APIKey = "c27b6f158393dd9e97b1661d03e463b8";
       
     const searchValue = $("#search-field").val().trim();
     
+    const search = $("#search-field").val();
+    
     const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${APIKey}&units=metric`;
+
+    
   
       fetch(queryURL)
       .then(function (response) {
@@ -77,11 +84,61 @@ const APIKey = "c27b6f158393dd9e97b1661d03e463b8";
         $("#day6wind").text(((data.list[39].wind.speed)*3.6).toPrecision(2));
         $("#day6humidity").text(data.list[39].main.humidity);
         $("#iconday6").css({"background-image":`url(https://openweathermap.org/img/wn/${data.list[39].weather[0].icon}@2x.png)`});
-      })
-              
-      });
+      })   
+      setLocal(search);
+    }
+            );
   
+      
     //script for saving previous searches to local storage and updating previous search history.
+
+
+function pageFunction() {
+  counterValue = localStorage.getItem("count");
+  console.log(counterValue)
+}
+
+//this function shall run in side click event function once new search made.
+
+function setLocal(Search) {
+  counterValue = localStorage.getItem("count");
+  if (counterValue === null || counterValue === 0) 
+  {
+    counterValue = 1
+    localStorage.setItem (counterValue, Search);
+    counterValue = 2
+    localStorage.setItem ("count", counterValue);
+  getLocalStorage(); 
+  }
+  else if (counterValue < 7) 
+  {
+    localStorage.setItem (counterValue, Search);
+    counterValue = parseInt(counterValue) + parseInt(1);
+    localStorage.setItem ("count", counterValue);
+  getLocalStorage();
+  } 
+  else {
+    counterValue = 1;
+    localStorage.setItem (counterValue, Search);
+    counterValue = parseInt(counterValue) + parseInt(1);
+    localStorage.setItem ("count", counterValue);
+  getLocalStorage();
+  }
+}
+
+function getLocalStorage() {
+  for (let i = 0; i < searchIdArr.length; i++) {
+  var inputContent = localStorage.getItem(searchIdArr[i]);
+  $("#"+ searchIdArr[i]).attr('value', inputContent)
+    }
+    }
+     
+  $( document ).ready(pageFunction());
+
+//when search performed populates search dependent on counter value e.g. if 1 populates 1 if 2 also sets local storage to same value, counter restarts when reaches more than 6 (number of buttons) counter is saved and stored in local storage and retrieved.
+
+
+//function for populating all previous search results runs at page refresh and once new value added.
 
 
 
